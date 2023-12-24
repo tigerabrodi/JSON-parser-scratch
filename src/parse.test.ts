fa62afa2b6f03df7f7ed60669e3445e776ccba33
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-24 12:13:52 +0100

    setup failing test

--- src/parse.test.ts
+++ src/parse.test.ts
@@ -1,3 +1,22 @@
-describe('JSON parse', () => {
-  describe
+import { describe, expect, it } from 'vitest'
+import { parse } from './parse'
+
+describe.skip('JSON parse from scratch', () => {
+  describe('primitive types', () => {
+    it('parses null', () => {
+      expect(parse('null')).toBe(null)
+    })
+    it('parses true', () => {
+      expect(parse('true')).toBe(true)
+    })
+    it('parses false', () => {
+      expect(parse('false')).toBe(false)
+    })
+    it('parses numbers', () => {
+      expect(parse('42')).toBe(42)
+    })
+    it('parses strings', () => {
+      expect(parse('"hello"')).toBe('hello')
+    })
+  })
 })
