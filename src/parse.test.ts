776aa48b6311c4b4cecb47c1f7ef5cede26dd31a
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-24 13:50:25 +0100

    throw invalid string

--- src/parse.test.ts
+++ src/parse.test.ts
@@ -23,5 +23,36 @@ describe('JSON parse from scratch', () => {
     it('parses strings', () => {
       expect(parse('"hello"')).toBe('hello')
     })
+
+    it('throws an error for invalid string', () => {
+      expect(() => parse('hello')).toThrow()
+    })
+  })
+
+  describe.skip('objects', () => {
+    it('parses an empty object', () => {
+      expect(parse('{}')).toEqual({})
+    })
+
+    // it('parses an object with a single key-value pair', () => {
+    //   expect(parse('{"key": "value"}')).toEqual({ key: 'value' })
+    // })
+
+    // it('parses an object with multiple key-value pairs', () => {
+    //   expect(parse('{"key1": "value1", "key2": 42}')).toEqual({
+    //     key1: 'value1',
+    //     key2: 42,
+    //   })
+    // })
+
+    // it('parses nested objects', () => {
+    //   expect(parse('{"outerKey": {"innerKey": "innerValue"}}')).toEqual({
+    //     outerKey: { innerKey: 'innerValue' },
+    //   })
+    // })
+
+    // it('throws an error for invalid objects', () => {
+    //   expect(() => parse('{"key": "value"')).toThrow()
+    // })
   })
 })
