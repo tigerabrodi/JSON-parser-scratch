b4b01cd0433b404a8659670005f4add182ef5a42
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-24 13:47:40 +0100

    refactor clean code

--- src/parse.ts
+++ src/parse.ts
@@ -1,14 +1,25 @@
 // building JSON parse from scratch
+
+const NUMBER_REGEX = /^-?\d+(\.\d+)?$/
+
+function isNumber(value: string): boolean {
+  return NUMBER_REGEX.test(value)
+}
+
+function parseString(input: string): string {
+  // Check for starting and ending quotes
+  if (input.startsWith('"') && input.endsWith('"')) {
+    return input.slice(1, -1)
+  }
+
+  throw new Error('Invalid string')
+}
+
 export function parse(input: string) {
   if (input === 'null') return null
   if (input === 'true') return true
   if (input === 'false') return false
-  if (Number(input)) return Number(input)
+  if (isNumber(input)) return Number(input)
 
-  // Needed to remove the quotes from the string that come with the input
-  // This removes the first and last character from the string
-  // e.g. '"hello"' -> 'hello'
-  // 1 means start at the second character
-  // -1 means stop at the second to last character
-  return input.slice(1, -1)
+  return parseString(input)
 }
