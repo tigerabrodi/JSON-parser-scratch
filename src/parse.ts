58e1624985b665f3064fa8d34a301ec9cc286451
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-24 13:44:18 +0100

    comments

--- src/parse.ts
+++ src/parse.ts
@@ -6,5 +6,9 @@ export function parse(input: string) {
   if (Number(input)) return Number(input)
 
   // Needed to remove the quotes from the string that come with the input
+  // This removes the first and last character from the string
+  // e.g. '"hello"' -> 'hello'
+  // 1 means start at the second character
+  // -1 means stop at the second to last character
   return input.slice(1, -1)
 }
