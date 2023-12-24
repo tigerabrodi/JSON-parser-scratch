cf9bab49b20babf9c5d26ee1e6b0bacd8f03d96b
Tiger Abrodi <tigerabrodi@gmail.com>
2023-12-24 14:20:25 +0100

    documentation

--- src/parse.ts
+++ src/parse.ts
@@ -27,12 +27,25 @@ export function parse(input: string) {
   if (input.startsWith(LEFT_BRACE) && input.endsWith(RIGHT_BRACE)) {
     if (input === '{}') return {}
 
-    const insideObject = input.slice(1, -1)
+    const insideObject = input.slice(1, -1) // '"key1": "value1", "key2": 42'
 
+    // 1. split by comma -> ['"key1": "value1"', '"key2": 42']
+    // 2. split by colon -> [['"key1"', ' "value1"'], ['"key2"', ' 42']]
+    // 3. trim whitespace -> [['"key1"', '"value1"'], ['"key2"', '42']]
     const objectKeysValues = insideObject
       .split(',')
       .map((pair) => pair.split(':').map((part) => part.trim()))
 
+    // 4. parse key and value -> [['key1', 'value1'], ['key2', 42]]
+    // 5. reduce to object -> { key1: 'value1', key2: 42 }
+
+    // How reduce works step by step:
+    // const object = {}
+    // object['key1'] = 'value1'
+    // object['key2'] = 42
+    // Both values for each key are parsed
+    // We have to use `parse` for values because they can be of any type
+    // return object
     return objectKeysValues.reduce<Record<string, unknown>>(
       (obj, [key, value]) => {
         obj[parseString(key)] = parse(value)
